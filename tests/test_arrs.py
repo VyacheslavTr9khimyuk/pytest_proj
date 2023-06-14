42db9b164bfd7048bbea7c230874ae0c306bba7e
Vyacheslav Tr9khimyuk <tva2023github@gmail.com>
2023-06-14 22:59:01 +0300

    modified: tests/test_arrs.py coverage 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,3 +11,8 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1, 2, 3], 1, 2) == [2]
+    assert arrs.my_slice([1, 2, 3], -3, -1) == [1, 2]
+    assert arrs.my_slice([1, 2, 3], -7,) == [1, 2, 3]
+    assert arrs.my_slice([], 1) == []
+
